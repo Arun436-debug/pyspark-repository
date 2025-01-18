6542358fe953e5b59db00cfc6e8626c3de6c22f6
Arun436-Debug <arunkandukuri94@gmail.com>
2025-01-18 16:32:20 +0530

    modified spark code from Arun-Second-Git-account

--- spark_code.py
+++ spark_code.py
@@ -6,4 +6,6 @@ output = spark.read.format('csv').option('header','true')\
     .option('path',"C:/Users/Administrator/Desktop/mycsv/employee_data_with_col_names.csv")\
     .load()
 
-spark.sql('select * from output').show()
+output.createOrReplaceTempView('employee')
+
+spark.sql('select * from employee').show()
